1bfc77478aaf8a0cac07509ebef8734bfc4e7ef6
ck198 <charmevillanueva1@gmail.com>
2025-04-26 13:35:10 +0800

    Create arrayACTIVITY.js

---
+++ arrayACTIVITY.js
@@ -0,0 +1,18 @@
+// Create an array called favSingers and store your three favorite singers in it.
+let favSingers = ["Darlene Zschech", "Joth Hunt", " Phil Wickham"];
+
+// Log the first singer in the array using array index notation.
+console.log (favSingers[0]);
+
+// Create an array called favNumbers and store four of your favorite numbers.
+let favNumbers = [2, 0, 7, 22];
+
+// Create an array named mixedArr and store different data types: ["string", ["otherarray"], 123, true].
+let mixedArr = ["Computer", ["Laptop"], 13, true];
+
+// Access and log each item in the mixedArr using array index notation.
+console.log(mixedArr[0]);
+console.log(mixedArr[1]); 
+console.log(mixedArr[1][0]);
+console.log(mixedArr[2]); 
+console.log(mixedArr[3]);
